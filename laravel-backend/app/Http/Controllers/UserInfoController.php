ef2c8141c6915dd678e5bebbba7842da42d80a55
lijion <lijion@indiana.edu>
2018-03-30 16:40:37 +1100

    eliminate non useful comment

--- laravel-backend/app/Http/Controllers/UserInfoController.php
+++ laravel-backend/app/Http/Controllers/UserInfoController.php
@@ -6,44 +6,15 @@ use App\User;
 use App\UserInfo;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-
-
 use Exception;
 use Validator;
->>>>>>> 18b13cdd9a86278ae0dcdec0583b3c219c54f507
+
 class UserInfoController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+    
+   
     public function store(Request $request)
     {
-
-//        try{
-
         $validator = Validator::make($request->all(), [
             'address' => 'required|string|unique:users',
             'email' => 'required|email|unique:users',
@@ -66,52 +37,6 @@ class UserInfoController extends Controller
 
             return response()->json(['error' => $e->getMessage()], 404);
         }
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\UserInfo  $userInfo
-     * @return \Illuminate\Http\Response
-     */
-    public function show(UserInfo $userInfo)
-    {
-        //
-
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\UserInfo  $userInfo
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(UserInfo $userInfo)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\UserInfo  $userInfo
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, UserInfo $userInfo)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\UserInfo  $userInfo
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(UserInfo $userInfo)
-    {
-        //
     }
 
     public function checkIfUserExists($address){
