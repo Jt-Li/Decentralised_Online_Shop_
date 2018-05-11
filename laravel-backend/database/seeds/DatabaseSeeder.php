3bd8c45f892276438267d212dbbb2fc8aaafdd54
Aaron <aaroncareer.au@gmail.com>
2018-05-11 14:15:30 +1000

    da

--- laravel-backend/database/seeds/DatabaseSeeder.php
+++ laravel-backend/database/seeds/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
         $this->call(UsersTableSeeder::class);
         $this->call(CategoriesTableSeeder::class);
         $this->call(ProductsTableSeeder::class);
-        $this->call(shopping_cartsTableSeeder::class);
+        // $this->call(shopping_cartsTableSeeder::class);
 
     }
 }
--- laravel-backend/database/seeds/UsersTableSeeder.php
+++ laravel-backend/database/seeds/UsersTableSeeder.php
@@ -14,24 +14,24 @@ class UsersTableSeeder extends Seeder
     {
         //
         User::create([
-            'name' => str_random(10),
+            'name' => 'Rex',
             'email' => 'test@gmail.com',
-            'address' => '0xd7f56927b3d89ac99622ddbfad9372c503d883ef'
+            'address' => '0xD7f56927b3d89ac99622dDbFad9372C503D883Ef'
         ]);
 
         User::create([
-            'name' => str_random(10),
+            'name' => 'JT1',
             'email' => 'test2@gmail.com',
             'address' => '0xE43Ddc0887818Cc2715a99F2cD98d422B603c55a'
         ]);
         User::create([
-            'name' => str_random(10),
+            'name' => 'JT2',
             'email' => 'test3@gmail.com',
             'address' => '0x997d5cd92af4A320A99047cC64f9ae1C985Efb7E'
         ]);
 
         User::create([
-            'name' => str_random(10),
+            'name' => 'Aaron',
             'email' => 'test4@gmail.com',
             'address' => '0x77346ca9e39DB4740f5f0C23A4b548157CC7Bfe8'
         ]);
